aee9f1ee9cc9149b81e632058b0e06bf8399f0cb
aravind <vijaravind@gmail.com>
2012-02-29 17:51:57 +0800

    set indentation to 4 spaces. removed reading serial number from YAML file. gets serial number using ros parameter. renamed packlib to g_packlib and initialized it as 0. changed logging level of some messages. changed ros::spin() to ros::spinOnce().

--- src/packbot.cpp
+++ src/packbot.cpp
@@ -14,12 +14,10 @@
 #include <sstream>
 #include <string>
 
-#include <yaml-cpp/yaml.h>
-
 #include <packbot/PackbotDef.hh>
 #include <packbot/Packbot.hh>
 
-#include "ros/ros.h"
+#include <ros/ros.h>
 
 #include "packbot_ros/PackbotState.h"
 #include "packbot_ros/ArmPose.h"
@@ -32,31 +30,7 @@ using namespace std;
 
 
 /// the object to communicate with libpackbot defined as global
-Packbot::Messenger *packlib;
-
-
-
-/// this function reads the yaml file and gets the serial number of the packbot
-unsigned int read_yaml( char *yaml_file )
-{
-	ifstream fin( yaml_file ); 
-	if ( fin.fail() ) 
-	{
-	  ROS_ERROR( "could not open %s which contains the serial number.\n", yaml_file );
-	  exit(-1);
-	}
-	YAML::Parser parser( fin );   
-	YAML::Node doc;
-	parser.GetNextDocument( doc );
-	unsigned int sno;
-	try { doc["serial"] >> sno; } 
-	catch (YAML::InvalidScalar) 
-	{ 
-	  printf( "the yaml file, %s does not contain the serial number for packbot.", yaml_file );
-	  exit(-1);
-	}
-	return sno;
-}
+Packbot::Messenger *g_packlib = 0;
 
 
 
@@ -65,45 +39,45 @@ packbot_ros::Arm get_packbot_arm_state()
 {
 	packbot_ros::Arm a;	
 	// turret
-	a.turret.joint 			= "turret";
-	a.turret.joint_id 	= 0;
-	a.turret.position 	= packlib->getArmJointPos( Packbot::JointID::turret );
-	a.turret.velocity 	= packlib->getArmJointVel( Packbot::JointID::turret );
+	a.turret.joint 		= "turret";
+	a.turret.joint_id	= 0;
+	a.turret.position	= g_packlib->getArmJointPos( Packbot::JointID::turret );
+	a.turret.velocity	= g_packlib->getArmJointVel( Packbot::JointID::turret );
 	// shoulder
-	a.shoulder.joint 		= "shoulder";
-	a.shoulder.joint_id = 1;
-	a.shoulder.position = packlib->getArmJointPos( Packbot::JointID::shoulder );
-	a.shoulder.velocity = packlib->getArmJointVel( Packbot::JointID::shoulder );	
+	a.shoulder.joint	= "shoulder";
+	a.shoulder.joint_id	= 1;
+	a.shoulder.position	= g_packlib->getArmJointPos( Packbot::JointID::shoulder );
+	a.shoulder.velocity	= g_packlib->getArmJointVel( Packbot::JointID::shoulder );	
 	// elbow1
-	a.elbow1.joint 			= "elbow1";
-	a.elbow1.joint_id 	= 2;
-	a.elbow1.position 	= packlib->getArmJointPos( Packbot::JointID::elbow1 );
-	a.elbow1.velocity		= packlib->getArmJointVel( Packbot::JointID::elbow1 );	
+	a.elbow1.joint 		= "elbow1";
+	a.elbow1.joint_id	= 2;
+	a.elbow1.position	= g_packlib->getArmJointPos( Packbot::JointID::elbow1 );
+	a.elbow1.velocity	= g_packlib->getArmJointVel( Packbot::JointID::elbow1 );	
 	// elbow2
-	a.elbow2.joint 			= "elbow2";
-	a.elbow2.joint_id 	= 3;
-	a.elbow2.position 	= packlib->getArmJointPos( Packbot::JointID::elbow2 );
-	a.elbow2.velocity 	= packlib->getArmJointVel( Packbot::JointID::elbow2 );	
+	a.elbow2.joint 		= "elbow2";
+	a.elbow2.joint_id	= 3;
+	a.elbow2.position	= g_packlib->getArmJointPos( Packbot::JointID::elbow2 );
+	a.elbow2.velocity	= g_packlib->getArmJointVel( Packbot::JointID::elbow2 );	
 	// tilt
-	a.tilt.joint 				= "tilt";
-	a.tilt.joint_id 		= 4;
-	a.tilt.position 		= packlib->getArmJointPos( Packbot::JointID::tilt );
-	a.tilt.velocity 		= packlib->getArmJointVel( Packbot::JointID::tilt );	
+	a.tilt.joint 		= "tilt";
+	a.tilt.joint_id 	= 4;
+	a.tilt.position 	= g_packlib->getArmJointPos( Packbot::JointID::tilt );
+	a.tilt.velocity 	= g_packlib->getArmJointVel( Packbot::JointID::tilt );	
 	// pan
-	a.pan.joint 				= "pan";
-	a.pan.joint_id 			= 5;
-	a.pan.position 			= packlib->getArmJointPos( Packbot::JointID::pan );
-	a.pan.velocity 			= packlib->getArmJointVel( Packbot::JointID::pan );
+	a.pan.joint 		= "pan";
+	a.pan.joint_id 		= 5;
+	a.pan.position 		= g_packlib->getArmJointPos( Packbot::JointID::pan );
+	a.pan.velocity 		= g_packlib->getArmJointVel( Packbot::JointID::pan );
 	// wrist
-	a.wrist.joint 			= "wrist";
-	a.wrist.joint_id 		= 6;
-	a.wrist.position 		= packlib->getArmJointPos( Packbot::JointID::wrist );
-	a.wrist.velocity 		= packlib->getArmJointVel( Packbot::JointID::wrist );	
+	a.wrist.joint 		= "wrist";
+	a.wrist.joint_id 	= 6;
+	a.wrist.position	= g_packlib->getArmJointPos( Packbot::JointID::wrist );
+	a.wrist.velocity 	= g_packlib->getArmJointVel( Packbot::JointID::wrist );	
 	// grip
-	a.grip.joint 				= "grip";
-	a.grip.joint_id 		= 7;
-	a.grip.position 		= packlib->getArmJointPos( Packbot::JointID::grip );
-	a.grip.velocity 		= packlib->getArmJointVel( Packbot::JointID::grip );	
+	a.grip.joint 		= "grip";
+	a.grip.joint_id 	= 7;
+	a.grip.position 	= g_packlib->getArmJointPos( Packbot::JointID::grip );
+	a.grip.velocity 	= g_packlib->getArmJointVel( Packbot::JointID::grip );	
 	return a;
 }
 
@@ -114,7 +88,7 @@ void callback_velocity( const packbot_ros::Velocity::ConstPtr& msg )
 {
 	float v_lin = (float)msg->linear;
 	float v_ang = (float)msg->angular;
-	packlib->setVelocity( v_lin, v_ang );
+	g_packlib->setVelocity( v_lin, v_ang );
 	ROS_INFO( "set the velocity of packbot to lin = %f, ang = %f", v_lin, v_ang );
 }
 
@@ -125,8 +99,8 @@ void callback_brakes( const packbot_ros::Brakes::ConstPtr& msg )
 {
 	bool b_main = (bool)msg->main;
 	bool b_arm  = (bool)msg->arm;
-	packlib->setMainBrake( b_main );
-	packlib->setArmBrake( b_arm );
+	g_packlib->setMainBrake( b_main );
+	g_packlib->setArmBrake( b_arm );
 	ROS_INFO( "set the brakes of packbot to main = %d, arm = %d", b_main, b_arm );
 }
 
@@ -136,7 +110,7 @@ void callback_brakes( const packbot_ros::Brakes::ConstPtr& msg )
 void callback_flipper( const packbot_ros::Flipper::ConstPtr& msg )
 {
 	float vel = (float)msg->velocity;
-	packlib->setFlipperVel( vel );
+	g_packlib->setFlipperVel( vel );
 	ROS_INFO( "set the flipper velocity to vel = %f", vel );
 }
 
@@ -146,7 +120,7 @@ void callback_flipper( const packbot_ros::Flipper::ConstPtr& msg )
 void callback_light( const packbot_ros::Misc::ConstPtr& msg )
 {
 	bool on = msg->light;
-	packlib->setAttackLight( on, false );
+	g_packlib->setAttackLight( on, false );
 	ROS_INFO( "set the packbot action light to %d", on );
 }
 
@@ -156,7 +130,7 @@ void callback_light( const packbot_ros::Misc::ConstPtr& msg )
 void callback_camera_tilt( const packbot_ros::Misc::ConstPtr& msg )
 {
 	int tilt = msg->camera_tilt;
-	packlib->setDriveCameraTiltDegrees( tilt, false );
+	g_packlib->setDriveCameraTiltDegrees( tilt, false );
 	ROS_INFO( "set the drive camera tilt to tilt = %d", tilt );
 }
 
@@ -167,7 +141,7 @@ void callback_armjoint_vel( const packbot_ros::ArmJoint::ConstPtr& msg )
 {
 	int joint_id = msg->joint_id;
 	float vel    = (float)msg->velocity;
-	packlib->setArmJointVel( (Packbot::JointID::JointId)joint_id, vel );
+	g_packlib->setArmJointVel( (Packbot::JointID::JointId)joint_id, vel );
 	ROS_INFO( "set joint %s to speed %f", (msg->joint).c_str(), vel );
 }
 
@@ -177,7 +151,7 @@ void callback_armjoint_vel( const packbot_ros::ArmJoint::ConstPtr& msg )
 void callback_arm_pose( const packbot_ros::ArmPose::ConstPtr& msg )
 {
 	int pose = msg->arm_pose;
-	packlib->setArmPose( (Packbot::ArmPose::ArmPose)pose, false );
+	g_packlib->setArmPose( (Packbot::ArmPose::ArmPose)pose, false );
 	ROS_INFO( "set packbot arm to position %d", pose );
 }
 
@@ -188,34 +162,29 @@ int main( int argc, char **argv )
 {
 
 	ros::init( argc, argv, "packbot" );
-	
-	if( argc < 2 )
-	{
-		printf( "\n\tUSAGE:\n\t\t%s <path to the yaml file with the serial number>\n", argv[0] );
-		exit( -1 );
-	}
-	
-	unsigned int serial = read_yaml( argv[1] );
-	
-	packlib = new Packbot::Messenger( serial );
-	
-	if( !packlib->initialize() )
-	{
-		ROS_ERROR( "unable to initialize libpackbot..." );
-		ROS_INFO( "more details need to be reported later if possible..." );
-		ros::shutdown();
-	}
-	
 	ros::NodeHandle n;
+
+	int serial = 12099;
+	if( !n.getParam( "serial", serial ) )
+		ROS_INFO( "using the default Packbot serial number - 12099." );
+	
+	g_packlib = new Packbot::Messenger( (unsigned int)serial );
+	
+	if( !g_packlib->initialize() )
+	{
+		ROS_FATAL( "unable to initialize libpackbot..." );
+		// more details need to be reported later if possible...
+		ros::shutdown();
+	}	
 	
 	/// call all the subscribers to the different topics
-	ros::Subscriber vel_sub = n.subscribe( "packbot_set_velocity", 			10, callback_velocity );
-	ros::Subscriber bks_sub = n.subscribe( "packbot_set_brakes",   			10, callback_brakes );
-	ros::Subscriber flp_sub = n.subscribe( "packbot_set_flipper",  			10, callback_flipper );
-	ros::Subscriber lgt_sub = n.subscribe( "packbot_set_light",    			10, callback_light );
-	ros::Subscriber cmt_sub = n.subscribe( "packbot_set_camera_tilt", 	10, callback_camera_tilt );
-	ros::Subscriber ajv_sub = n.subscribe( "packbot_set_armjoint_vel", 	10, callback_armjoint_vel );
-	ros::Subscriber aps_sub = n.subscribe( "packbot_set_arm_pose", 			10, callback_arm_pose );
+	ros::Subscriber vel_sub = n.subscribe( "packbot_set_velocity",		10, callback_velocity );
+	ros::Subscriber bks_sub = n.subscribe( "packbot_set_brakes", 		10, callback_brakes );
+	ros::Subscriber flp_sub = n.subscribe( "packbot_set_flipper",		10, callback_flipper );
+	ros::Subscriber lgt_sub = n.subscribe( "packbot_set_light",			10, callback_light );
+	ros::Subscriber cmt_sub = n.subscribe( "packbot_set_camera_tilt",	10, callback_camera_tilt );
+	ros::Subscriber ajv_sub = n.subscribe( "packbot_set_armjoint_vel",	10, callback_armjoint_vel );
+	ros::Subscriber aps_sub = n.subscribe( "packbot_set_arm_pose",		10, callback_arm_pose );
 	
 	/// setup the publisher
 	ros::Publisher pbs_pub = n.advertise<packbot_ros::PackbotState>( "packbot_state", 10 );
@@ -230,38 +199,34 @@ int main( int argc, char **argv )
 	unsigned int count = 0;
 	
 	while( ros::ok() )
-	{
-	
-		packlib->getVelocity( v_lin, v_ang );
-		packlib->getPose( yaw, roll, pitch );
+	{	
+		g_packlib->getVelocity( v_lin, v_ang );
+		g_packlib->getPose( yaw, roll, pitch );
 		
 		/// assign the values to the PackbotState msg datatype
-		pbs.velocity.linear				= v_lin;
-		pbs.velocity.angular			= v_ang;
-		pbs.orientation.roll			= roll;
-		pbs.orientation.pitch			= pitch;
-		pbs.orientation.yaw				= yaw;
-		pbs.brakes.main						= packlib->isMainBrakeEngaged();
-		pbs.brakes.arm						= packlib->isArmBrakeEngaged();
-		pbs.flipper.position			= packlib->getFlipperPos();
-		pbs.flipper.velocity			= packlib->getFlipperVel();
+		pbs.velocity.linear		= v_lin;
+		pbs.velocity.angular	= v_ang;
+		pbs.orientation.roll	= roll;
+		pbs.orientation.pitch	= pitch;
+		pbs.orientation.yaw		= yaw;
+		pbs.brakes.main			= g_packlib->isMainBrakeEngaged();
+		pbs.brakes.arm			= g_packlib->isArmBrakeEngaged();
+		pbs.flipper.position	= g_packlib->getFlipperPos();
+		pbs.flipper.velocity	= g_packlib->getFlipperVel();
 		
 		/// get the state of the packbot's arm
-		pbs.arm										= get_packbot_arm_state();
+		pbs.arm					= get_packbot_arm_state();
 		
 		/// create the header for the msg
-		pbs.header.seq 						= count;
-		pbs.header.stamp 					= ros::Time::now();
-		pbs.header.frame_id 			= "base_link";
+		pbs.header.seq 			= count++;
+		pbs.header.stamp 		= ros::Time::now();
+		pbs.header.frame_id 	= "packbot_frame";
 		
 		/// publish the msg
 		pbs_pub.publish( pbs );
 		
-		count++;
-		
-		ros::spin();
+		ros::spinOnce();
 		loop_rate.sleep();
-	
 	}
 	
 	return 0;
